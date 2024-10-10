3ff94743b4ffd5a20daa500b108f95cbd598a49c
ryohamag <Ryojune21@gmail.com>
2024-10-10 17:35:22 +0900

    カテゴリランキング関数を作成

---
+++ app/src/main/java/com/example/recipegenerator/components/CategoryRanking.kt
@@ -0,0 +1,9 @@
+package com.example.recipegenerator.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CategoryRanking() {
+    Text(text = "h_ryoはここで作業します。")
+}
