7927840bdaf9693b5f863537b44c746493424c75
suzukibelltree <suzuki879455@gmail.com>
2025-02-04 23:15:46 +0900

    APIから取得するデータの種類を増やした

--- app/src/main/java/com/example/recipegenerator/Result.kt
+++ app/src/main/java/com/example/recipegenerator/Result.kt
@@ -4,16 +4,27 @@ import com.google.gson.annotations.SerializedName
 
 //APIから取得したデータを格納するためのデータクラス
 data class ApiData(
+    // レシピのタイトル
     @SerializedName("recipeTitle")
     val Title: String,
+    // レシピのURL
     @SerializedName("recipeUrl")
     val url: String,
+    // レシピの画像URL
     @SerializedName("foodImageUrl")
     val foodimage: String,
+    // レシピの費用
     @SerializedName("recipeCost")
     val cost: String,
+    // レシピの調理時間
     @SerializedName("recipeIndication")
-    val indication: String
+    val indication: String,
+    // 投稿者のニックネーム
+    val nickname: String,
+    // レシピの説明コメント
+    val recipeDescription: String,
+    // レシピの材料(リスト)
+    val recipeMaterial: List<String>,
 )
 
 //ResultDataのプロパティにお気に入り登録されたかを表す変数を追加したデータクラス(UI表示に関してはこちらを操作する)
