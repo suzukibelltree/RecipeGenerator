b72ec4441959cf3b64e622b5b81d1b8de0753054
ryohamag <Ryojune21@gmail.com>
2025-02-12 10:40:29 +0900

    検索に合致するカテゴリを取得する関数を作成

--- app/src/main/java/com/example/recipegenerator/ViewModel/CategoryViewModel.kt
+++ app/src/main/java/com/example/recipegenerator/ViewModel/CategoryViewModel.kt
@@ -2,6 +2,7 @@ package com.example.recipegenerator.ViewModel
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import com.example.recipegenerator.DataClass.Large
 import com.example.recipegenerator.DataClass.Medium
@@ -24,5 +25,16 @@ class CategoryViewModel : ViewModel() {
     var isMediumCategoryLoaded = mutableStateOf(false) // 中カテゴリが読み込まれたかどうか
     var isSmallCategoryLoaded = mutableStateOf(false) // 小カテゴリが読み込まれたかどうか
     val searchCategoryText = mutableStateOf("") // 検索されたカテゴリ名
+    var matchLargeCategoryList = mutableStateListOf<Large>() // 検索された大カテゴリのリスト
+    var matchMediumCategoryList = mutableStateListOf<Medium>() // 検索された中カテゴリのリスト
+    var matchSmallCategoryList = mutableStateListOf<Small>() // 検索された小カテゴリのリスト
+
+    //合致したカテゴリを返す関数
+    fun getMatchCategoryList() {
+        val searchCategory = searchCategoryText.value
+        matchLargeCategoryList = largeList.filter { it.categoryName.contains(searchCategory) } as SnapshotStateList<Large>
+        matchMediumCategoryList = mediumList.filter { it.categoryName.contains(searchCategory) } as SnapshotStateList<Medium>
+        matchSmallCategoryList = smallList.filter { it.categoryName.contains(searchCategory) } as SnapshotStateList<Small>
+    }
 
 }
